100e6d5b152d105fe95f29ea5f5e1265795e4146
HastG99 <nik_hastov.2020@mail.ru>
2022-08-23 14:51:29 +0300

    Update 1.1

--- src/main/java/ru/hastg9/fapplugin/listeners/PlayerListener.java
+++ src/main/java/ru/hastg9/fapplugin/listeners/PlayerListener.java
@@ -3,23 +3,27 @@ package ru.hastg9.fapplugin.listeners;
 import org.bukkit.*;
 import org.bukkit.boss.BarColor;
 import org.bukkit.boss.BossBar;
-import org.bukkit.entity.Entity;
-import org.bukkit.entity.EntityType;
-import org.bukkit.entity.Player;
+import org.bukkit.entity.*;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
+import org.bukkit.event.entity.ProjectileHitEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.event.player.PlayerMoveEvent;
+import org.bukkit.metadata.FixedMetadataValue;
 import org.bukkit.potion.PotionEffectType;
 import ru.hastg9.fapplugin.FapPlugin;
 import ru.hastg9.fapplugin.managers.FapManager;
 import ru.hastg9.fapplugin.utils.StringUtils;
 
+import java.util.HashSet;
 import java.util.Random;
+import java.util.Set;
 
 public class PlayerListener implements Listener {
+
+    private final Set<Player> shooters = new HashSet<>();
 
     @EventHandler
     public void onClick(PlayerInteractEvent event) {
@@ -53,20 +57,16 @@ public class PlayerListener implements Listener {
             Random random = new Random();
             if(random.nextInt(4) == 3) {
                 FapManager.setFapper(player, -1);
+                shooters.add(player);
 
                 Bukkit.getScheduler().runTaskAsynchronously(FapPlugin.getInstance(), () -> {
                     for (int i = 0; i < 3; i++) {
                         world.playSound(player.getLocation(), Sound.ENTITY_PLAYER_SWIM, 5, 2);
 
-                        player.spawnParticle(Particle.SPIT, player.getEyeLocation(), 10, 0, 1, 0);
-
                         Location location = player.getLocation().toVector().add(player.getLocation().getDirection().multiply(0.8D)).
                                 toLocation(player.getWorld()).add(0.0D, 1.0D, 0.0D);
 
-                        Bukkit.getScheduler().runTask(FapPlugin.getInstance(), () -> {
-                            Entity spitmonster = player.getWorld().spawnEntity(location, EntityType.LLAMA_SPIT);
-                            spitmonster.setVelocity(player.getLocation().getDirection().multiply(2));
-                        });
+                        Bukkit.getScheduler().runTask(FapPlugin.getInstance(), () -> spawnShootProjectile(player, location));
 
                         try {
                             Thread.sleep(450);
@@ -94,6 +94,14 @@ public class PlayerListener implements Listener {
         FapManager.setFapper(player, ++count);
     }
 
+
+    private void spawnShootProjectile(Player player, Location location) {
+        LlamaSpit spitmonster = (LlamaSpit) player.getWorld().spawnEntity(location, EntityType.LLAMA_SPIT);
+        spitmonster.setMetadata("cm_shoot", new FixedMetadataValue(FapPlugin.getInstance(), player.getName()));
+        spitmonster.setShooter(player);
+        spitmonster.setVelocity(player.getLocation().getDirection().multiply(2));
+    }
+
     @EventHandler
     public void onAttack(EntityDamageByEntityEvent event) {
         if(event.getDamager() instanceof Player) {
@@ -107,6 +115,13 @@ public class PlayerListener implements Listener {
 
         if(event.getEntity() instanceof Player) {
             Player player = (Player) event.getEntity();
+
+            if(event.getDamager() instanceof LlamaSpit) {
+                LlamaSpit spit = (LlamaSpit) event.getDamager();
+
+                if(spit.hasMetadata("cm_shoot"))
+                    event.setDamage(0.0);
+            }
 
             if(!FapManager.hasFapper(player))
                 return;
@@ -127,6 +142,27 @@ public class PlayerListener implements Listener {
 
         if(FapManager.hasFapper(event.getPlayer()))
             event.setCancelled(true);
+    }
+
+    @EventHandler
+    public void onProjectileHit(ProjectileHitEvent event) {
+        Projectile projectile = event.getEntity();
+        Entity entity = event.getHitEntity();
+
+        if(projectile instanceof LlamaSpit && entity instanceof Player) {
+            Player target = (Player) entity;
+
+            if(!(projectile.getShooter() instanceof Player)) return;
+
+            Player damager = (Player) projectile.getShooter();
+
+            if(!shooters.contains(damager)) return;
+
+            damager.sendMessage(String.format(FapPlugin.getMessage("messages.hit-success"), target.getName()));
+            target.sendMessage(FapPlugin.getMessage("messages.were-hit"));
+
+            shooters.remove(damager);
+        }
     }
 
     private boolean isLookAround(Location from, Location to) {
